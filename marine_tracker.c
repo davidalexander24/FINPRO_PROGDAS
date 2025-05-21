26f30e14990d1f8b24c8eb73223139b9b98b0230
David Alexander <89135811+davidalexander24@users.noreply.github.com>
2025-05-21 22:32:11 +0700

    Revert "David: Add type definitions and dynamic-array init/ensure_capacity"

--- marine_tracker.c
+++ marine_tracker.c
@@ -2,40 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-// --- Types ---
-typedef enum { FISH, MAMMAL, CRUSTACEAN, MOLLUSK } SpeciesType;
-
-typedef union {
-    float averageWeight;
-    int typicalCount;
-} ExtraInfo;
-
-typedef struct {
-    char name[50];
-    SpeciesType type;
-    float length;
-    ExtraInfo info;
-} MarineRecord;
-
-// --- Globals & Array Management ---
-int capacity = 10;
-int count = 0;
-MarineRecord *records;
-
-void init_array() {
-    records = malloc(sizeof(MarineRecord) * capacity);
-    if (!records) { perror("malloc"); exit(EXIT_FAILURE); }
-}
-
-void ensure_capacity() {
-    if (count >= capacity) {
-        capacity *= 2;
-        records = realloc(records, sizeof(MarineRecord) * capacity);
-        if (!records) { perror("realloc"); exit(EXIT_FAILURE); }
-    }
-}
 
 //tambahin function baru di atas main
 int main() {
-    
+
 }
